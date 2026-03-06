30e5b7aefc791423fec1028adb78e168d42a45bb
richarai9 <36673660+richarai9@users.noreply.github.com>
2026-03-06 16:44:19 -0600

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/TechImmersion_Flowchart/",
+  base: "/Tech-Immersion-Flowchart/",
   plugins: [react(), tailwindcss()],
 });
